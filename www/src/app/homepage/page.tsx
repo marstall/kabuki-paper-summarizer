ac35aeef07851a2c875835431d5c35198069589f
Chris Marstall <marstall@gmail.com>
2026-02-25 15:18:04 -0500

    tweeaks for build

--- www/src/app/homepage/page.tsx
+++ www/src/app/homepage/page.tsx
@@ -1,5 +1,7 @@
 import Homepage from "./homepage"
 
+export const dynamic = 'force-dynamic'
+
 export default function Page() {
   return <Homepage />
 }
--- www/src/app/page.tsx
+++ www/src/app/page.tsx
@@ -1,5 +1,7 @@
 import Homepage from "@/app/homepage/homepage";
 
+export const dynamic = 'force-dynamic'
+
 export default function Home() {
   return <Homepage/>
 }
