47986129553e70ceb4b20d494acd55cad62eb0ff
Chris Marstall <marstall@gmail.com>
2026-02-26 11:55:52 -0500

    bring header to translation page, other css tweaks

--- www/src/app/components/header/header.tsx
+++ www/src/app/components/header/header.tsx
@@ -1,16 +1,31 @@
 import styles from './header.module.css'
+import Link from "next/link";
 
-export default function Header(params) {
+export default function Header({minimal}) {
   return <div className={styles.container}>
-    <div className={styles.logo}>The Kabuki Papers</div>
+    <div className={styles.logo}>
+      <Link href='/'>The Kabuki Papers</Link>
+
+    </div>
     <div className={styles.slogan}>
       Making the science behind Kabuki Syndrome more accessible, one paper at a time
     </div>
-    <div className={styles.credits}>
-      Kabuki-related academic papers translated into plain English by AI.<br/>
-      Tap any sentence to show source passages.<br/>
-      Our Editor and AI wrangler is Chris Marstall, a curious Kabuki parent.<br/>
-    </div>
+    {!minimal && <div className={styles.credits}>
+      <ul className={styles.creditsList}>
+        <li className={styles.creditItem}>
+          <span className={styles.creditIcon}>🧬</span>
+          <span className={styles.creditText}>Kabuki-related academic papers translated into plain English by AI.</span>
+        </li>
+        <li className={styles.creditItem}>
+          <span className={styles.creditIcon}>🔍</span>
+          <span className={styles.creditText}>Tap any sentence to show source passages.</span>
+        </li>
+        <li className={styles.creditItem}>
+          <span className={styles.creditIcon}>👨</span>
+          <span className={styles.creditText}>Our Editor and AI wrangler is Chris Marstall, a curious Kabuki parent.</span>
+        </li>
+      </ul>
+    </div>}
   </div>
 
 }
