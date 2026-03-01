17eeaa612305be59cc11359f2bf2ad2148fbf461
Chris Marstall <marstall@gmail.com>
2026-03-01 05:22:39 -0500

    add vercel speed insights

--- www/src/app/layout.tsx
+++ www/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "./article.css";
 import SubscribeForm from "@/app/components/subscribe-form/subscribe-form";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
