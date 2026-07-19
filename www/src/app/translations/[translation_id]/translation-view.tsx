354b7567e0cf44c09d8c979a644b9546a472d6dd
chris marstall <marstall@gmail.com>
2026-07-19 08:41:53 -0400

    fix ts error

--- www/src/app/translations/[translation_id]/translation-view.tsx
+++ www/src/app/translations/[translation_id]/translation-view.tsx
@@ -1,17 +1,19 @@
-import TranslationViewClient from "@/app/components/translation-view-client/translation-view-client";
+import TranslationViewClient
+    from "@/app/components/translation-view-client/translation-view-client";
 import _ from 'lodash'
 import {loadTranslation} from '@/app/lib/load-translation'
 
 export default async function TranslationView({translation_id}: any) {
-  const translation = await loadTranslation(translation_id)
-  if (!translation) return <div>Translation Not found</div>
+    const translation = await loadTranslation(translation_id)
+    if (!translation) return <div>Translation Not found</div>
 
-  return <TranslationViewClient
-    translation={translation}
-    article={translation.articles}
-    llm={translation.llms}
-    promptTitle={_.get(translation, "prompts.title")}
-    attachments={_.get(translation,'articles.attachments',[])}
-  />
+    return <TranslationViewClient
+        showAttachmentCaptions={false}
+        translation={translation}
+        article={translation.articles}
+        llm={translation.llms}
+        promptTitle={_.get(translation, "prompts.title")}
+        attachments={_.get(translation, 'articles.attachments', [])}
+    />
 
 }
